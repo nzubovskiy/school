961b999b71e909359a986e4e81ec962e0091f651
NIKO <nzubovskiy@gmail.com>
2022-08-27 14:55:49 +0400

    Homework_3.3

---
+++ src/main/java/ru/hogwarts/school/StudentService.java
@@ -0,0 +1,44 @@
+package ru.hogwarts.school;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student editStudent(long l, Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public List<Student> findByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+
+}
